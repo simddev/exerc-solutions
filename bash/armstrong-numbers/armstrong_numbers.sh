9243ff51e5cef7d834ad8eaf06d108b04fd2f16e
simddev <175357996+simddev@users.noreply.github.com>
2026-06-16 19:46:15 +0200

    arsmtrong solved

--- bash/armstrong-numbers/armstrong_numbers.sh
+++ bash/armstrong-numbers/armstrong_numbers.sh
@@ -1,24 +1,21 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+input="$1"
+
+length=${#input}
+
+sum=0
+
+for (( i=0; i<length; i++ ))
+do
+    digit=${input:i:1}
+
+    sum=$((sum + digit ** length))
+done
+
+if (( sum == input ))
+then
+    echo "true"
+else
+    echo "false"
+fi
