89ef736fb60f11b65c4f8496f019bdcc40db34d2
simddev <175357996+simddev@users.noreply.github.com>
2026-06-20 15:18:46 +0200

    grains solved

--- bash/grains/grains.sh
+++ bash/grains/grains.sh
@@ -2,22 +2,33 @@
 
 input="$1"
 
-if [[ $# -ne 1 ]]
-then
+if [[ $# -ne 1 ]]; then
     echo "Error: invalid input"
     exit 1
 fi
 
-if [[ $input == "total" ]]
-then
-    echo $(( (2 ** 64) - 1 ))
+if [[ $input == "total" ]]; then
+    sum=0
+    value=1
+
+    for (( i=1; i<=64; i++ )); do
+        sum=$((sum + value))
+        value=$((value * 2))
+    done
+
+    # IMPORTANT: force unsigned interpretation
+    printf "%u\n" "$sum"
     exit 0
 fi
 
-if [[ $input -lt 1 || $input -gt 64 ]]
-then
+if [[ $input -lt 1 || $input -gt 64 ]]; then
     echo "Error: invalid input"
     exit 1
 fi
 
-echo $(( 2 ** (input - 1) ))
+value=1
+for (( i=1; i<input; i++ )); do
+    value=$((value * 2))
+done
+
+printf "%u\n" "$value"
