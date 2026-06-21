874f9b4fea56eb339619127bd69b30094880a4e9
simddev <175357996+simddev@users.noreply.github.com>
2026-06-21 13:54:06 +0200

    solved hamming

--- bash/hamming/hamming.sh
+++ bash/hamming/hamming.sh
@@ -1,24 +1,21 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+[[ $# -eq 2 ]] || {
+    echo "Usage: hamming.sh <string1> <string2>"
+    exit 1
+}
+
+if [[ ${#1} -ne ${#2} ]]; then
+    echo "strands must be of equal length"
+    exit 1
+fi
+
+count=0
+
+for ((i = 0; i < ${#1}; i++)); do
+    if [[ "${1:i:1}" != "${2:i:1}" ]]; then
+        ((count++))
+    fi
+done
+
+echo "$count"
