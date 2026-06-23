30a12c6d84a0459b3abc44bdc8eb5f1811f187cf
simddev <175357996+simddev@users.noreply.github.com>
2026-06-23 10:44:28 +0200

    house working

--- bash/house/house.sh
+++ bash/house/house.sh
@@ -1,45 +1,58 @@
 #!/usr/bin/env bash
 
 subjects=(
-"house that Jack built"
-"malt"
-"rat"
-"cat"
-"dog"
-"cow with the crumpled horn"
-"maiden all forlorn"
-"man all tattered and torn"
-"priest all shaven and shorn"
-"rooster that crowed in the morn"
-"farmer sowing his corn"
-"horse and the hound and the horn"
+    "house that Jack built"
+    "malt"
+    "rat"
+    "cat"
+    "dog"
+    "cow with the crumpled horn"
+    "maiden all forlorn"
+    "man all tattered and torn"
+    "priest all shaven and shorn"
+    "rooster that crowed in the morn"
+    "farmer sowing his corn"
+    "horse and the hound and the horn"
 )
 
 verbs=(
-"lay in"
-"ate"
-"killed"
-"worried"
-"tossed"
-"milked"
-"kissed"
-"married"
-"woke"
-"kept"
-"belonged to"
+    "lay in"
+    "ate"
+    "killed"
+    "worried"
+    "tossed"
+    "milked"
+    "kissed"
+    "married"
+    "woke"
+    "kept"
+    "belonged to"
 )
 
-for (( verse=0; verse<${#subjects[@]}; verse++ ))
-do
+[[ $# -eq 2 ]] || exit 1
+
+start=$1
+end=$2
+
+if ((start < 1 || end < 1 || start > 12 || end > 12 || start > end)); then
+    echo "invalid verse"
+    exit 1
+fi
+
+print_verse() {
+    local verse=$1
+
     echo "This is the ${subjects[verse]}"
 
-    for (( i=verse; i>0; i-- ))
-    do
-        echo "that ${verbs[i-1]} the ${subjects[i-1]}"
+    for ((i = verse; i > 0; i--)); do
+        echo "that ${verbs[i - 1]} the ${subjects[i - 1]}."
     done
+}
 
-    echo
+for ((v = start - 1; v < end; v++)); do
+    print_verse "$v"
 
+    if ((v < end - 1)); then
+        echo
+    fi
 done
-
-
