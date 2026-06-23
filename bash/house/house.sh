78534c472e2f0749e1cbd64604a51f2ca82060c7
simddev <175357996+simddev@users.noreply.github.com>
2026-06-23 10:18:46 +0200

    house attempt

--- bash/house/house.sh
+++ bash/house/house.sh
@@ -1,5 +1,45 @@
 #!/usr/bin/env bash
 
-input="$1"
+subjects=(
+"house that Jack built"
+"malt"
+"rat"
+"cat"
+"dog"
+"cow with the crumpled horn"
+"maiden all forlorn"
+"man all tattered and torn"
+"priest all shaven and shorn"
+"rooster that crowed in the morn"
+"farmer sowing his corn"
+"horse and the hound and the horn"
+)
+
+verbs=(
+"lay in"
+"ate"
+"killed"
+"worried"
+"tossed"
+"milked"
+"kissed"
+"married"
+"woke"
+"kept"
+"belonged to"
+)
+
+for (( verse=0; verse<${#subjects[@]}; verse++ ))
+do
+    echo "This is the ${subjects[verse]}"
+
+    for (( i=verse; i>0; i-- ))
+    do
+        echo "that ${verbs[i-1]} the ${subjects[i-1]}"
+    done
+
+    echo
+
+done
 
 
